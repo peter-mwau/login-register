ead2b6be352cddaf9bb91605bbf770d777851ed2
Peter Kyale <kyalepeter2000@gmail.com>
2023-07-20 08:49:29 +0300

    Modified UI

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,20 +2,50 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-// import { useState } from 'react';
+import { useState } from 'react';
 // import { auth } from '../../firebase/firebase'
 // import { createUserWithEmailAndPassowrd } from 'firebase/auth'
 
-export default function Login() {
+export default function Home() {
+  // const Signin = () => {
+  //   const [email, setEmail] = useState("")
+  //   const [password, setPassword] = useState("")
+  //   const [confirmpassword, setConfirmPassword] = useState("")
+  //   const [error, setError] = useState("")
+  //   const [success, setSuccess] = useState("")
+    
+  //   const register = (e) => {
+  //     e.preventDefault()
+  //     if (password !== confirmpassword) {
+  //       return setError("Passwords do not match")
+  //     }
+  //     auth.createUserWithEmailAndPassword(email, password).then((userCredential) => {
+  //       // Signed in 
+  //       var user = userCredential.user;
+  //       setSuccess("Account created successfully")
+  //       // ...
+  //     })
+  //       .catch((error) => {
+  //         var errorCode = error.code;
+  //         var errorMessage = error.message;
+  //         setError(errorMessage)
+  //         // ..
+  //       });
+  //   }
+  // }
+
 
   const toggle = () => {
     const password = document.getElementById('password');
-    if (password.getAttribute('type') === 'password') {
+    const confirmpassword = document.getElementById('confirmpassword')
+    if (password.getAttribute('type') === 'password' || confirmpassword?.getAttribute('type') === 'password') {
       password.setAttribute('type', 'text');
-     
+      confirmpassword?.setAttribute('type', 'text');
 
     } else {
       password.setAttribute('type', 'password');
+      confirmpassword?.setAttribute('type', 'password');
+
     }
   }
   return (
@@ -42,24 +72,38 @@ export default function Login() {
             <a href="/register" className="text-lg font-semibold hover:text-white hover:underline-offset-1">
               About
             </a>
-            <a href="/services" className="text-lg font-semibold p-2 rounded-lg hover:bg-white hover:text-black hover:shadow-md hover:transition-shadow hover:cursor-pointer outline-none">
+            <a href="/services" className="text-lg font-semibold hover:text-white hover:underline-offset-1">
               Services
             </a>
             <a href="/contact" className="text-lg font-semibold hover:text-white hover:underline-offset-1">
               Contact
             </a>
-            <a href="/register" className="text-lg font-semibold">
-              Register
+            <a href="/" className="text-lg font-semibold p-2 rounded-lg hover:bg-white hover:text-black hover:shadow-md hover:transition-shadow hover:cursor-pointer outline-none">
+              Login
             </a>
+           
           </div>
         </div>
       </nav>
       <div className="flex justify-center items-center h-full relative">
         <div className="bg-black bg-opacity-60 h-[400px auto] w-[500px] p-8 rounded-lg shadow-lg">
           {/* Add your login form components here */}
-          <h2 className="text-2xl font-bold mb-4 flex mx-auto items-center justify-center uppercase font-serif text-white">Login</h2>
+          <h2 className="text-2xl font-bold mb-4 flex mx-auto items-center justify-center uppercase font-serif text-white">Register</h2>
           <form>
-            <div className="space-y-4">
+            <div>
+            <label htmlFor="names" className="block text-sm font-medium text-white">
+                Names
+              </label>
+              <div className="mt-1">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="email"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm"
+                />
+              </div>
               <label htmlFor="email" className="block text-sm font-medium text-white">
                 Email address
               </label>
@@ -102,15 +146,40 @@ export default function Login() {
                 />
                 </div>
               </div>
+              <div className="mt-1 relative">
+              <label htmlFor="email" className="block text-sm font-medium text-white">
+                Confirm Password
+              </label>
+                <div className='flex items-center'>
+                <input
+                  id="confirmpassword"
+                  name="confirmpassword"
+                  type="password"
+                  autoComplete="email"
+                  required
+                  className="appearance-none block w-full px-3 py-2 pr-10 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm"
+                />
+                {/* <svg></svg> */}
+                <Image
+                  src="/eye-off.svg"
+                  alt="Background Image"
+                  width={20}
+                  height={20}
+                  className='absolute right-2 bottom-2 hover:cursor-pointer'
+                  onClick={toggle}
+                 
+                />
+                </div>
+              </div>
               <div className='mt-2'>
                 <input type='checkbox' className=''/>
                 <span className='m-2 p-2 text-lg space-x-2 text-white'>Remember Me</span>
               </div>
             </div>
             <div className='m-2 p-3 mx-auto items-center justify-center shadow-xlg flex'>
-              <button type='submit' className='bg-gray-50 text-xl p-1 rounded-sm font-serif font-extrabold hover:bg-gray-400 hover:delay-75 hover:shadow-lg'>Login</button>
+              <button type='submit' className='bg-gray-50 text-xl p-1 rounded-sm font-serif font-extrabold hover:bg-gray-400 hover:delay-75 hover:shadow-lg'>Register</button>
               {/* <a href='login' className='text-white text-md hover:underline m-2 '>Sign In</a> */}
-              <Link href={'/register'} className='text-white text-md hover:underline m-2 '>Sign Up</Link>
+              <Link href={'/'} className='text-white text-md hover:underline m-2 '>Sign In</Link>
             </div>
           </form>
         </div>
@@ -118,4 +187,3 @@ export default function Login() {
     </div>
   );
 }
-
